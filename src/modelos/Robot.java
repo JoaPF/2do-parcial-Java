2256f1faf8334fb0c6d43c76a224b1975a27fb69
Joa Pérez <100404436+JoaPF@users.noreply.github.com>
2025-11-20 12:10:00 -0300

    Update Robot.java

--- src/modelos/Robot.java
+++ src/modelos/Robot.java
@@ -9,7 +9,7 @@ public abstract class Robot {
         if (nivelEnergia < 0 || nivelEnergia > 100) {
             throw new ExcepcionRobot("El nivel de energía debe estar entre 0 y 100.");
         }
-        // Validación: solo números positivos
+        //numeros positivos
         if (!numeroSerie.matches("\\d+") || Integer.parseInt(numeroSerie) < 0) {
              throw new ExcepcionRobot("El número de serie debe ser un valor positivo.");
         }
@@ -19,7 +19,7 @@ public abstract class Robot {
         this.numeroSerie = numeroSerie;
     }
 
-    // Getters y Setters
+    // getters y setters
     public String getNombre() { return nombre; }
     public void setNombre(String nombre) { this.nombre = nombre; }
 
@@ -32,10 +32,10 @@ public abstract class Robot {
 
     public String getNumeroSerie() { return numeroSerie; }
     
-    // Métodos abstractos
     public abstract String obtenerTipo();
     public abstract String obtenerDetalleEspecifico();
     
     // Método para serializar a JSON manualmente
     public abstract String aJson();
+
 }
